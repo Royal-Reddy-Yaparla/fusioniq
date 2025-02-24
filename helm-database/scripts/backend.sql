3fbbcd7fcc79a22f98920a5b566fc9a9a4504c49
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-02-24 11:07:35 +0530

    fusion

--- helm-database/scripts/backend.sql
+++ helm-database/scripts/backend.sql
@@ -1,3 +1,6 @@
+-- Create the admin user with a password
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'Admin@123';
+
 -- Grant all privileges to the `admin` user
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
 
